01e1b97d7dacfee3ca39318a1ef5ebb9c477947f
Siew Wing Fei <siewwingfei@hotmail.com>
2019-08-08 17:12:32 +0800

    k

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
@@ -17,10 +18,51 @@ namespace sportsstore_api_dotnet_core.Controllers
             repository = repo;
         }
 
-        [HttpPost("checkout")]
-        public ActionResult<Dictionary<string, object>> Checkout(Dictionary<string, object> m)
+        [HttpGet]
+        public ActionResult<List<Order>> List()
         {
-            return m;
+            var q = repository.Orders.Where(o => !o.Shipped);
+            return q.ToList();
+        }
+
+        [HttpPost("checkout")]
+        public ActionResult<Order> Checkout(Dictionary<string, object> m)
+        {
+            Order o = new Order {
+                OrderID = 0,
+                Name = m["name"].ToString(),
+                Line1 = m["line1"].ToString(),
+                Line2 = m["line2"].ToString(),
+                Line3 = m["line3"].ToString(),
+                City = m["city"].ToString(),
+                State = m["state"].ToString(),
+                Zip = m["zip"].ToString(),
+                Country = m["country"].ToString(),
+                GiftWrap = Convert.ToBoolean(m["giftwrap"])
+            };
+            Newtonsoft.Json.Linq.JArray cartLines = (Newtonsoft.Json.Linq.JArray)m["cartLines"];
+            List<CartLine> lx = new List<CartLine>();
+
+            foreach (var x in cartLines)
+            {
+                var product = x["product"];
+                int quantity = (int)x["quantity"];
+                CartLine c = new CartLine();
+                c.Product = new Product {
+                    ProductID = (int)product["productID"],
+                    Name = product["name"].ToString(),
+                    Description = product["description"].ToString(),
+                    Price = (decimal)product["price"],
+                    Category = product["category"].ToString()
+                };
+                c.Quantity = quantity;
+                lx.Add(c);
+            }
+
+            o.Lines = lx;
+            repository.SaveOrder(o);
+            
+            return o;
         }
     }
 }
